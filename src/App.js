951936d7f5fdf44493fbaab238c2a5e5c6fdc041
Yuexin <yuexinli1203@gmail.com>
2024-09-28 23:58:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Element, animateScroll as scroll } from "react-scroll";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Element name="home" className="element">
+        <h1>Home Section</h1>
+      </Element>
+
+      <Element name="about" className="element">
+        <h1>About Section</h1>
+      </Element>
+
+      <Element name="work" className="element">
+        <h1>Work Section</h1>
+      </Element>
+
+      <Element name="skills" className="element">
+        <h1>Skills Section</h1>
+      </Element>
+
+      <Element name="contact" className="element">
+        <h1>Contact Section</h1>
+      </Element>
     </div>
   );
 }
